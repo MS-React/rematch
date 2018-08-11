c9eef641fc1238d27dc0e9803be952522e1d7ce7
Ramiro Gonzalez <rgonzalez@makingsense.com>
2018-08-11 10:56:21 -0300

    Adds proofGenerator test by mock Math api

---
+++ src/app/utils/proofGenerator.spec.js
@@ -0,0 +1,31 @@
+import { createProof } from './proofGenerator';
+
+describe('proofGenerator', () => {
+  let mathApi;
+
+  beforeAll(() => {
+    mathApi = global.Math;
+    const mockMath = Object.create(global.Math);
+    mockMath.floor = () => 1;
+    global.Math = mockMath;
+  });
+
+  afterAll(() => {
+    global.Math = mathApi;
+  });
+
+  describe('createProof', () => {
+    let proof;
+
+    beforeEach(() => {
+      proof = createProof();
+    });
+
+    it('should create a random aritmetic basic math proof and return the equation in a string and the result', () => {
+      expect(proof).toEqual(expect.objectContaining({
+        equation: '1 - 1',
+        result: 0
+      }));
+    });
+  });
+});
